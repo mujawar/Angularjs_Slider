72cc173dd0ac627e017180d978eb8960c630d605
arif <arifmr91@gmail.com>
2017-01-30 22:31:15 +0530

    html changes

--- mainApp.js
+++ mainApp.js
@@ -5,9 +5,7 @@ var mainApp = angular.module("mainApp", []);
 
 mainApp.controller('mySliderController', function($scope) {
     console.log('calling controller')
-
     $scope.slides = [
-
                      { img: 'http://res.cloudinary.com/http-arifmujawar-in/image/upload/v1477587246/image1_gyppqn.jpg', text : 'Nature always wears the colors of the spirit'},
                      { img: 'http://res.cloudinary.com/http-arifmujawar-in/image/upload/v1477587255/image2_ooxpnb.jpg', text : 'Float like a butterfly, sting like a bee'},
                      { img: 'http://res.cloudinary.com/http-arifmujawar-in/image/upload/v1477587256/image3_jayakx.jpg', text : 'Sunset is my favorite color, and rainbow is second'},
